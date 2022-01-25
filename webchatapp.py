c5670e8c56ff64ab12ae6c0bc95cacdc15e61447
Dante <dante142@gmail.com>
2022-01-24 19:01:34 -0800

    sign in page

--- webchatapp.py
+++ webchatapp.py
@@ -1,24 +1,23 @@
-from flask import Flask, render_template #flask imports
+from crypt import methods
+from flask import Flask, render_template, request #flask imports
+from flask_bootstrap import Bootstrap #bootstrap import
 import requests, json #currently unused
 from flask_socketio import SocketIO #used to connect users for chat
 
 app = Flask(__name__)
 import os
-SECRET_KEY = os.urandom(32)
+SECRET_KEY = os.urandom(32) #generate a random key
 app.config['SECRET_KEY'] = SECRET_KEY
+bootstrap = Bootstrap(app)
 SocketIO = SocketIO(app)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST']) #the main sign in screen
 def hello():
     return render_template('home.html')
 
-def received(methods = ['GET','POST']):
-    print('the message was received sucessfully')
-
-@SocketIO.on('event')
-def handle_message(message,methods=['GET','POST']):
-    print('received message:' + str(message))
-    SocketIO.emit('my message', message, callback=received)
+@app.route('/chat',methods=['GET','POST']) #the chat screen
+def chat():
+    return request.form['username']
 
 if __name__ == '__main__':
     app.debug= True
